64a1e3403ea02f72c44f0799656eff6a2c219960
Sebastian Ebner <sebastian@ebner.de>
2016-07-26 20:53:53 +0200

    create student model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726132412) do
+ActiveRecord::Schema.define(version: 20160726184441) do
+
+  create_table "students", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",   null: false
