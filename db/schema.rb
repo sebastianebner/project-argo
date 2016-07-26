f8aa6b6c31bc830643bcea2651b8eb972c9aec8e
Sebastian Ebner <sebastian@ebner.de>
2016-07-26 21:14:07 +0200

    made student user assoc.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160726184441) do
+ActiveRecord::Schema.define(version: 20160726190217) do
 
   create_table "students", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_students_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
