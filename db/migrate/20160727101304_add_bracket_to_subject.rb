8f251a3684a6e7938b8bd9e8577f81a2206861d0
Sebastian Ebner <sebastian@ebner.de>
2016-07-27 12:13:59 +0200

    added reference for bracket to subjects

---
+++ db/migrate/20160727101304_add_bracket_to_subject.rb
@@ -0,0 +1,5 @@
+class AddBracketToSubject < ActiveRecord::Migration[5.0]
+  def change
+  	add_reference :subjects, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727095642) do
+ActiveRecord::Schema.define(version: 20160727101304) do
 
   create_table "brackets", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20160727095642) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_students_on_user_id"
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_subjects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
